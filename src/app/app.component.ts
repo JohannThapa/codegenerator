8dc8bb68595eb4a9ca8b0b4ca6808a64ca62fa0f
Johann Thapa <38758416+JohannThapa@users.noreply.github.com>
2019-10-19 20:01:28 +0545

    function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codegenerator';
+  date = new Date();
+  codeGenerated = '';
+  evtMsg: any;
+  randomString() {
+ const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+ const stringLength = 10;
+ let randomstring = '';
+ for (let i = 0; i < stringLength; i++) {
+ const rnum = Math.floor(Math.random() * chars.length);
+ randomstring += chars.substring(rnum, rnum + 1);
+}
+ this.codeGenerated = randomstring;
+ return 0;
+}
 }
